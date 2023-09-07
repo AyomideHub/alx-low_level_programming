705da1431ad6941cdbe8b67cfa23e5c565e98610
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 16:37:14 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
-     *program to print using the function printf
+     *program to print using the function puts
      *and it return 0
 */
 	int main(void)
 {
-   	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

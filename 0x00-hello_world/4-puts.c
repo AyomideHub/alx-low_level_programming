6e1ddc6b9718a3b349a2c8be478f60a18784257f
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 16:09:08 -0700

    new task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
      *program to print using the function puts
      *and it return 0
 */
-           int main(void)
+	int main(void)
 {
-           puts("\"Programming is like building a multilingual puzzle");
-
-           return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

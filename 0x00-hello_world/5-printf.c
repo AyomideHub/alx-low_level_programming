50db227eb9ddada7751aa01b8495cf23515b2fe8
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 16:17:53 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+     *program to print using the function printf
+     *and it return 0
+*/
+	int main(void)
+{
+   	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

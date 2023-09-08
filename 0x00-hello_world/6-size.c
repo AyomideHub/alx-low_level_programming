a51fbaa920a7e8ac36b67e1575167f437c666910
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 17:25:14 -0700

    6new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /*
-	*main program that print the size of various types on the computer it is compiled and run on.
-	*and it return 0
+	*main- program that print the size of various type on the computer
+	*it is compiled and run on.
+	*and it return 0 (success)
 */
 	int main(void)
 {
@@ -10,11 +11,11 @@
 	int b;
 	long int c;
 	long long int d;
-	float e;
-	printf("sizes of a char: $lu byte(s)\n ", (unsigned long) sizeof(a));
-	printf("sizes of an int: $lu byte(s)\n ", (unsigned long) sizeof(b));
-	printf("sizes of a long int: $lu byte(s)\n ", (unsigned long) sizeof(c));
-	printf("sizes of a long long int: $lu byte(s)\n ", (unsigned long) sizeof(d));
-	printf("sizes of a float: $lu byte(s)\n ", (unsigned long) sizeof(e));
+	float f;
+	printf("sizes of a char: $lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("sizes of an int: $lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("sizes of a long int: $lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("sizes of a long long int: $lu byte(s)\n", (unsigned long) sizeof(d));
+	printf("sizes of a float: $lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }

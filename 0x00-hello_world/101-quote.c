57d9bdfa996b6a525df21f436dbac0bb014ec872
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 17:52:39 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+	**main - UNIX is basically a simple operating system
+	*Return: 1 means success
+*/
+	int main(void)
+{
+	fputs ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

5b6b626e56cc692a354a797ab6ed1481776f5084
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-07 18:57:14 -0700

    updated documebtation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main: task 5 c programming hello world
- * main function is an int to return and print a program with printf
+ ** main function is an int to return and print a program with printf
  * Description: function program to print without using puts
  * Return: always 0 success
  */

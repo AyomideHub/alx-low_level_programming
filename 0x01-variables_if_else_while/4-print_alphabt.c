09587420f186b44335bcef84c9ba64722598c51d
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-08 19:17:02 -0700

    task 4nww

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,21 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 /**
  * Main - Entry point
  * Return: always 0 success
  */
 
-int main() {
-    char x;
-    x = 'a';
-    
-    while
-    (x <= 'z')
-    {
-        if ((x != 'q' && x != 'e') && x <= 'z')
-        putchar(x);
-	x++;
-    }
-    putchar('\n');
+int main(void)
+{
+	char x;
 
-    return 0;
+	x = 'a';
+	while
+		(x <= 'z')
+		{
+			if ((x != 'q' && x != 'e') && x <= 'z')
+			putchar(x);
+			x++;
+}
+	putchar('\n');
+	return (0);
 }

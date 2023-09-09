006ef42420f279c0d011c3731a15529cfe076481
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-08 19:24:57 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+        int i = 0;
+        
+        while (i < 10)
+        {
+           printf ("%i\n", i++);
+        }
+        
+        return (0);
+}

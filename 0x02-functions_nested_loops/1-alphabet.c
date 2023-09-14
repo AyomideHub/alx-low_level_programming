d0e857cd05d7611e9b57bfd1c74a6d0c0f5f94ce
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-13 20:19:50 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function that print the alphatbet in lower case, followed by new line
+ * Return: 0 alwways
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+		_putchar('i');
+
+		_putchar(' ');
+
+
+}

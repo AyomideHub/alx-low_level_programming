e4ac46416f7b106e53eb9fd731d9b86e2551fdbf
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-13 21:41:18 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - funtion that add two integers and return the result
+ *@int - parameter to be checked
+ *Return: int 
+ */
+
+int add(int a, int b)
+{
+	int result; 
+	result = (a + b);
+	return (result);
+
+
+}

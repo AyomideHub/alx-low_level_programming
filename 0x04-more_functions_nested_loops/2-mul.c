9a5295e423dec46174b7e24c89d825424765302a
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-15 19:53:54 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function that multipliers to interger
+ * @a - parameter 1
+ * @b - parameter 2
+ * Return: 1 on success 0 on failure
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+
+
+}

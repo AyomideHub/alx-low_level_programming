eb0b5f90c77962ff61754012f95c62b5e01404fc
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-15 20:59:13 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print number 0 to i4, 10 times
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+
+		}
+		_putchar('\n');
+
+	}
+
+		_putchar('\n');
+
+
+}

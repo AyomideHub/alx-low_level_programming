af188a61b9f78f0cd4692723fd2d054a34522f0b
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-15 21:15:16 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line - function yhat draw a straight line
+ * @n - number of time '_' will be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++
+				{
+					_putchar('_');
+					_putchar('\n');
+
+				}
+
+
+	}
+
+
+
+
+}

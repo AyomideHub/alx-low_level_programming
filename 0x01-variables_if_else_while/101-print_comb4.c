fad131c71fb8b8ac3dd4d8ebe9e526f6cf2269ec
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-17 10:39:31 -0700

    task 11 update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-	char h = '0', t = '1', u = '2';
+	int h = 0, t = 1, u = 2;
 
-	while ((h <= '7') || (t <= '8') || (u <= '9'))
+	while ((h <= 7) || (t <= 8) || (u <= 9))
 	{
-		putchar(h);
-		putchar(t);
-		putchar(u);
+		putchar(h + '0');
+		putchar(t + '0');
+		putchar(u + '0');
 		putchar(',');
 		putchar(' ');
-	u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
+	u != 9 ? ++u : (t != 8 ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
 	}
 	return (0);
 }

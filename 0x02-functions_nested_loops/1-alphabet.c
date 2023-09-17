870936d757ca0af2f67dfcdd6f5674583dee008a
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-17 09:23:55 -0700

    task 1 new check

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * main - function that print the alphatbet in lower case, followed by new line
+ * print_alphabet - function that print the alphatbet in lower case, followed by new line
  * Return: 0 alwways
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-
-		_putchar('i');
-
-		_putchar('\n');
-
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 
 }

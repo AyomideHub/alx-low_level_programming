11469d3c8cfab952df54c9618b50141871c81c5a
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-17 09:43:09 -0700

    task 10 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * Main - program
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(i + '0');
+			_putchar(j + '0');
+		}
+
+	}
+
+
+
+
+
+}

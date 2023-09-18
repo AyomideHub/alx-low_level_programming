933fb995ea154b86040ba2f4c198a671df5630b4
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-18 13:40:54 -0700

    task 2 working

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string to be counted
+ * Return: 1 on success
+ */
 
 int _strlen(char *s)
 {
 	int i;
-	int stcount;
+	int stcount = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 		stcount++;

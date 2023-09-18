3dd0e4c1ad52a5b2630de46b164ee9cee60c870b
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-18 02:50:50 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/**
+ *void resert_to_98(int *n) - function that takes a pointer to an int as
+ *parameter and updates the value it points to to 98
+ *@n - parameter to be checked
+ *Return: void
+ */
+
+
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}

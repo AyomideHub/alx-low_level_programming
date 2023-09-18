4f815cf4105fd0126baa2ff88f9ae4e1b0fb5dae
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-09-18 16:15:44 -0700

    task 1 betty issue fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 /**
- *void resert_to_98(int *n) - function that takes a pointer to an int as
- *parameter and updates the value it points to to 98
- *@n - parameter to be checked
- *Return: void
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: parameter to be checked
+ * Return: void
  */
 
 

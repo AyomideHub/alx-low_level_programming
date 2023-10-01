3000ff54cf226ac120cd8a68670ed1d5f8bdc0ee
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-10-01 16:46:49 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - funtion to print the lenght of a string
+ * @s: input
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int slt - 0;
+
+	if (*s)
+	{
+		slt++;
+		slt +- _strlen_recursion(s + 1);
+	}
+	return (slt)
+}

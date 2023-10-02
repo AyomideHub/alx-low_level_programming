01f4c57158cd5eb70e3541f7733ad7b324742511
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-10-01 17:10:39 -0700

    task 2 beety issue fiexd

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - funtion that print a string in reverse 
+ * _print_rev_recursion - funtion that print a string in reverse
  * @s: input
  */
 
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 	_print_rev_recursion(s + 1);
 	_putchar(*s);

e9c82efabbaf57a0eb9f7f5925fa914883411bb3
Dada Ayomide Victor <dadaayomide64@gmail.com@example.com>
2023-10-09 21:54:18 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - function that allocate memory using malloc
+ * @b: size of memmory to be allocated
+ * Return: ponter to the memory allocated
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *O;
+
+	O = malloc(b);
+
+	if (O == NULL)
+		exit(98)
+	else
+		return(O);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,9 @@
+0x0C. C - More malloc, fre
+
+
+
+task 0: 
+Write a function that allocates memory using
+
+
+
